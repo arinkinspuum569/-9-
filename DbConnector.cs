f08e77d5de4ec6f0783b4491e15b073c58ac612e
User <arina.belyaeva.17@bk.ru>
2024-06-10 09:58:23 +0300

    Добавьте файлы проекта.

---
+++ DbConnector.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace билет_9_экзамен
+{
+    
+        internal static class DbConnector
+        {
+            private static readonly string ConnStr = "Server=localhost;DataBase=ekzDb;port=3306;User Id=root;password=";
+            private static readonly MySqlConnection Conn = new MySqlConnection(ConnStr);
+
+            static DbConnector()
+            {
+                Conn.Open();
+            }
+
+            public static long AddUser(User user)
+            {
+                try
+                {
+                    try
+                    {
+                        Conn.Open();
+                    }
+                    catch { }
+                    var cmd = Conn.CreateCommand();
+                    cmd.CommandText = "INSERT INTO `users` (login, password) VALUES (@l, @p)";
+                    cmd.Parameters.Add(new MySqlParameter("@l", user.Login));
+                    cmd.Parameters.Add(new MySqlParameter("@p", user.Password));
+                    cmd.ExecuteNonQuery();
+                    return cmd.LastInsertedId;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return -1;
+                }
+                finally { Conn.Close(); }
+            }
+
+        public static long AddStats(Stats stats)
+        {
+            try
+            {
+                try
+                {
+                    Conn.Open();
+                }
+                catch { }
+                var cmd = Conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO `online_stats` (login_time, logout_time) VALUES (@l, @p)";
+                cmd.Parameters.Add(new MySqlParameter("@l", stats.LoginTime));
+                cmd.Parameters.Add(new MySqlParameter("@p", stats.LogoutTime));
+                cmd.ExecuteNonQuery();
+                return cmd.LastInsertedId;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -1;
+            }
+            finally { Conn.Close(); }
+        }
+
+        public static bool Check(User user)
+            {
+                try
+                {
+                    Conn.Open();
+                }
+                catch { }
+
+                var cmd = Conn.CreateCommand();
+                cmd.CommandText = "SELECT password FROM `users` WHERE login = @l";
+                cmd.Parameters.Add(new MySqlParameter("@l", user.Login));
+                var res = cmd.ExecuteReader();
+                try
+                {
+                    while (res.Read())
+                    {
+                        var value = res.GetString(0);
+                        if (value is string pass)
+                        {
+                            if (user.Password == pass)
+                            {
+
+                                return true;
+                            }
+                        }
+                    }
+
+                    return false;
+
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); return false; }
+                finally
+                {
+                    Conn.Close();
+                }
+            }
+        }
+    }
---
+++ Stats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace билет_9_экзамен
+{
+    internal class Stats
+    {
+        public int LoginTime { get; set; }
+
+        public int LogoutTime { get; set; }
+
+
+        public Stats(int loginTime, int logoutTime)
+        {
+            LoginTime = loginTime;
+            LogoutTime = logoutTime;
+        }
+    }
+}
